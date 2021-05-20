d1c9f8f6581cfcde3154961869dcbd85140e208c
looi <looi@users.noreply.github.com>
2021-05-20 16:02:17 -0700

    Fix entrypoint.sh 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
+export HOME=/home/airflow
+export PATH="$HOME/.local/bin:$PATH"
 export AIRFLOW_HOME=${GITHUB_WORKSPACE}
 export AIRFLOW__CORE__DAGS_FOLDER=${GITHUB_WORKSPACE}/
 export AIRFLOW__CORE__PLUGINS_FOLDER=${GITHUB_WORKSPACE}/plugins/
 export AIRFLOW__CORE__LOGS_FOLDER="/tmp/logs/"
 export FERNET_KEY=$(openssl rand -base64 32)
 whoami
+echo $PATH
 airflow initdb
 python /main.py
