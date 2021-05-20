c66a7c4cd179b75ee679850120e302b5a3be69ba
looi <looi@users.noreply.github.com>
2021-05-20 16:33:38 -0700

    Testing changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ export AIRFLOW__CORE__DAGS_FOLDER=${GITHUB_WORKSPACE}/
 export AIRFLOW__CORE__PLUGINS_FOLDER=${GITHUB_WORKSPACE}/plugins/
 export AIRFLOW__CORE__LOGS_FOLDER="/tmp/logs/"
 export FERNET_KEY=$(openssl rand -base64 32)
+cat $GITHUB_EVENT_PATH
 airflow initdb
 python /main.py
