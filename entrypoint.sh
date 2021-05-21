519afb9a1235f429d47ade9b177a073457284eac
looi <looi@users.noreply.github.com>
2021-05-20 20:03:57 -0700

    Add image

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ export AIRFLOW__CORE__DAGS_FOLDER=${GITHUB_WORKSPACE}/
 export AIRFLOW__CORE__PLUGINS_FOLDER=${GITHUB_WORKSPACE}/plugins/
 export AIRFLOW__CORE__LOGS_FOLDER="/tmp/logs/"
 export FERNET_KEY=$(openssl rand -base64 32)
+RESULTS_DIR=${GITHUB_WORKSPACE}/airflow-diff-results
 echo Base ref is $GITHUB_BASE_REF
 airflow initdb
 python /dump_dags.py /tmp/current
@@ -14,10 +15,13 @@ python /dump_dags.py /tmp/base
 DAG_IDS=$(basename -a /tmp/base/* /tmp/current/* | sort | uniq)
 SUMMARY=""
 for dag_id in $DAG_IDS; do
-    if [[ ! -f "/tmp/base/$dag_id" ]]; then
-        SUMMARY+="**DAG added: $dag_id**"$'\n\n'
-    elif [[ ! -f "/tmp/current/$dag_id" ]]; then
+    if [[ ! -f "/tmp/current/$dag_id" ]]; then
         SUMMARY+="**DAG deleted: $dag_id**"$'\n\n'
+        continue
+    fi
+    if [[ ! -f "/tmp/base/$dag_id" ]]; then
+        CONTENT=$(< /tmp/base/$dag_id)
+        SUMMARY+="**DAG added: $dag_id**"$'\n\n```\n'"$CONTENT"$'\n```\n\n'
     else
         DIFF=$(diff -u /tmp/base/$dag_id /tmp/current/$dag_id)
         retVal=$?
@@ -25,6 +29,8 @@ for dag_id in $DAG_IDS; do
             SUMMARY+="**DAG modified: $dag_id**"$'\n\n```\n'"$DIFF"$'\n```\n\n'
         fi
     fi
+    cp /tmp/current/$dag_id $RESULTS_DIR/$dag_id.txt
+    airflow show_dag -s $RESULTS_DIR/$dag_id.png $dag_id
 done
 SUMMARY="${SUMMARY//'%'/'%25'}"
 SUMMARY="${SUMMARY//$'\n'/'%0A'}"
